d1414c3cb862956f3192cb4aeb8e64235d698bdd
Dbreazi <157401683+Dbreazi@users.noreply.github.com>
2024-02-22 10:50:45 +0100

    Task 1.3 - Adding Loops and Conditionals
    In this commit I added a 'for' loop to iterate over the current Pokemon and set a Conditional to highlight any Pokemon with a size >5.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,16 @@
+
 let PokemonList= [
     {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
     {name: 'Froslass', height: 3, type: ['ice', 'ghost']},
     {name: 'Horsea', height: 1, type: ['water', 'racing']}
-]
+];
+
+for (let i=0; i<PokemonList.length; i++) {
+    document.write(`${PokemonList[i].name} (height: ${PokemonList[i].height})`);
+
+    if (PokemonList[i].height >5 ){
+    document.write (" - Wow, that's a big one!");
+    }
+
+    document.write('<br>');
+}
