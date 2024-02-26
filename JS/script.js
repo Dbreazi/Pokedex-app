2d4b4b5e11085db84756e071edf77ab861fb1fee
Dbreazi <157401683+Dbreazi@users.noreply.github.com>
2024-02-26 21:10:29 +0100

    Task 1.5 pt 2 - adding IIFE
    wrapped PokemonList in IIFE // added return function // updated foreach loop

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,37 @@
 
-let PokemonList = [
-    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
-    {name: 'Froslass', height: 3, type: ['ice', 'ghost']},
-    {name: 'Horsea', height: 1, type: ['water', 'racing']}
+let pokemonRepo = (function () {
+    let PokemonList = [
+        {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+        {name: 'Froslass', height: 3, type: ['ice', 'ghost']},
+        {name: 'Horsea', height: 1, type: ['water', 'racing']}
 ];
 
-/*PokemonList.forEach (function(pokemon) {
-    document.write(pokemon.name + ' / Height: ' + pokemon.height + ' <br> ');
+return {
+    getAll: function() {
+        return PokemonList;
+    },
+
+    add: function (item) {
+        PokemonList.push(item);
+    }
+    };
+})();
+
+
+let pokemon = pokemonRepo.getAll();
+    pokemon.forEach(function(item) {
+        document.write(item.name + ' / Height: ' + item.height + (item.height > 5 ? " - Wow, that's a big one!" : "") + '<br>');
 });
-*/
+
+
+/* previous foreach loop
 
 function pokemonLoopFunction(pokemon) {
     document.write(pokemon.name + ' / Height: ' + pokemon.height + (pokemon.height > 5 ? " - Wow, that's a big one!" : "") + '<br>');
 
-    /* method 1 for >5 hieght rule
-    if (pokemon.height > 5 ) {
-        document.write (" - Wow, that's a big one!");   
-    }*/
+
 
 }
 
-
 PokemonList.forEach(pokemonLoopFunction);
+*/
