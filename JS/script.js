f7c35c1bc49b485b1b2ffc92806103aa9e43a674
Dbreazi <157401683+Dbreazi@users.noreply.github.com>
2024-03-05 22:34:53 +0100

    exercise 1.8 - added modal to JS
    added a modal to display pokemon attributes (with some difficulty)

--- JS/script.js
+++ JS/script.js
@@ -2,19 +2,23 @@ let pokemonRepo = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-    
+    // Function to add a Pokemon to the repository
     function add(pokemon) {
         if (
             typeof pokemon === 'object' && 'name' in pokemon
         ) {
             pokemonList.push(pokemon);
         } else {
-            console.log('pokemon is not correct');
+            console.error('pokemon is not correct');
         }
     }
+
+    // Function to get all Pokemon from the repository
     function getAll() {
         return pokemonList;
     }
+
+    // Function to add a list item for a Pokemon
     function addListItem(pokemon){
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement ('li');
@@ -28,6 +32,7 @@ let pokemonRepo = (function () {
         });
     }
     
+    // Function to load the list of Pokemon from the API
     function loadList () {
         return fetch(apiUrl).then(function (response) {
             return response.json();
@@ -40,11 +45,13 @@ let pokemonRepo = (function () {
                 add (pokemon);
                 console.log(pokemon);
             });
-        }).catch(function (e) {
+        })
+        .catch(function (e) {
             console.error(e);
         })
     }
 
+    // Function to load details of a specific Pokemon
     function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
@@ -53,21 +60,75 @@ let pokemonRepo = (function () {
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
             item.types = details.types;
-        }).catch(function (e) {
+        })
+        .catch(function (e) {
             console.error(e);
         });
     }
-    
-    // function showDetails(pokemon) {
-    //     console.log(pokemon);
-    // }
 
+
+    // Modal Code Start
+    let modalContainer = document.querySelector('#modal-container');
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
-            console.log(pokemon);
+            openModal(pokemon);
         });
     }
 
+    function openModal(pokemon) {
+        modalContainer.innerHTML = '';
+
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        let modalContent = document.createElement('div');
+        modalContent.classList.add('modal-content');
+
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        let nameElement = document.createElement('h2')
+        nameElement.innerText = pokemon.name;
+
+        let heightElement = document.createElement('p')
+        heightElement.innerText = `Height: ${pokemon.height} m`;
+
+        let imageElement = document.createElement('img');
+        imageElement.src = pokemon.imageUrl;
+        imageElement.alt = pokemon.name;
+
+        modalContent.appendChild(closeButtonElement);
+        modalContent.appendChild(nameElement);
+        modalContent.appendChild(heightElement);
+        modalContent.appendChild(imageElement);
+
+        modal.appendChild(modalContent)
+
+        modalContainer.appendChild(modal);
+        
+        modalContainer.classList.add('is-visible');
+    }
+
+    function hideModal () {
+        modalContainer.classList.remove('is-visible');
+    }
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            hideModal()
+        }
+    });
+
+    modalContainer.addEventListener('click', (e) => {
+        if(e.target === modalContainer) {
+            hideModal();
+        }
+    });
+
+    // Modal Code End
+        
     return {
         add: add,
         getAll: getAll,
@@ -77,11 +138,9 @@ let pokemonRepo = (function () {
     };
 })();
 
+// Load the list of Pokemon and add list items
 pokemonRepo.loadList().then(function() {
     pokemonRepo.getAll().forEach(function (pokemon) {
         pokemonRepo.addListItem(pokemon);
     });
 });
-
-
-
